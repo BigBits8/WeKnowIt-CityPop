773cb3238eba6c5c8dfd24708e6a6b5bf074d3a0
Mohammd Mustafa <momust187@gmail.com>
2022-06-14 21:19:53 +0200

    update: created bin

---
+++ bin/app.tsx
@@ -0,0 +1,22 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, View } from 'react-native';
+// import Main from './components/frontPage/main'
+// export default function App () {
+//   return (
+//     <View style={styles.container}>
+//       <Main />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     // borderColor: "#FF3D00",
+//     // borderWidth: 5,
+//     flex: 1,
+//     backgroundColor: '#eff9c8',
+//     alignItems: 'center',
+//     paddingTop: 150,
+//   },
+// });
