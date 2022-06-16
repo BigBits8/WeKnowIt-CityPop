a0c967ef1afb2d7cb65aa6d485000931b02fa3a5
Mohammd Mustafa <momust187@gmail.com>
2022-06-16 22:49:12 +0200

    API request: done

--- components/City/index.js
+++ components/City/index.js
@@ -1,22 +1,54 @@
-import {React, useState} from "react";
-import { Button, View, Text, TextInput, TouchableHighlight } from "react-native";
+import { React, useState, useEffect } from "react";
+import {
+  Button,
+  View,
+  Text,
+  TextInput,
+  TouchableHighlight,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import styles from "./style";
 import homeStyles from "../Home/style";
 import NavButton from "../Home/Buttons";
+
 let myText = "";
 
-
-const City = ({navigation}) => {
-  
+const City = ({ navigation }) => {
   const [text, setText] = useState("");
 
   const onSubmitEdit = () => {
     myText = `This is my text: ${text}`;
-    console.warn(myText)
-
+    console.warn(myText);
   };
+
+  let city = "Paris";
+  const url = `https://api.api-ninjas.com/v1/city?name=${city}`;
+  fetch(
+    url,
+    
+    {
+      method: "GET",
+      headers: {
+        "x-api-Key": "EtG+IKg8qrx8SIdtZep7Nw==6XWiAFigpduBbahC",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+    }
+  )
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  
+
+
+  
+  
   return (
     <View style={homeStyles.rel}>
       <View style={styles.wrapper}>
