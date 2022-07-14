68127c4b6b5041dcb6d79730463f058ee5e26c42
Mohammd Mustafa <momust187@gmail.com>
2022-07-14 19:04:43 +0200

    Handle error message when searching for city population, page ShowPop

--- components/showPop/index.tsx
+++ components/showPop/index.tsx
@@ -8,13 +8,14 @@ const ShowPop = () => {
   const [text, setText] = useState<string>();
   const [handleData, setHandleData] = useState([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<boolean>(true);
 
   // Get value from localstorage previously saved from 'Search by country' or 'Search by city' page
   const getData = async () => {
     try {
       const value = await AsyncStorage.getItem("@storage_Key");
       if (value !== null) {
-        console.warn(value);
+        // console.warn(value);
         setText(value);
       }
     } catch (e) {
@@ -23,7 +24,8 @@ const ShowPop = () => {
   };
 
   const fetchData = async () => {
-    getData();
+    try{
+       getData();
     const resp = await fetch(
       `https://api.api-ninjas.com/v1/city?name=${text}`,
       {
@@ -40,6 +42,19 @@ const ShowPop = () => {
 
     setHandleData(responseData);
     setLoading(false);
+    console.log(responseData.length);
+    // console.log(responseData);
+    const pop = responseData[0].population;
+    console.log(pop)
+    if(responseData[0].population != pop){
+      console.warn("no data found");
+    }else{
+      setErrorMessage(false)
+    }
+    }catch(err){
+      console.warn('fetch failed')
+    }
+   
   };
 
   type Item = {
@@ -62,15 +77,18 @@ const ShowPop = () => {
     <NativeBaseProvider>
       <View style={styles.rel}>
         <View style={styles.wrapper}>
-          <Text style={styles.text}>{text}</Text>
+          {(errorMessage && <Text style={styles.text}>oops..</Text>) || (
+            <Text style={styles.text}>{text}</Text>
+          )}
         </View>
 
         {handleData && (
           <View style={styles.pop}>
             <Text style={styles.popText}>Population</Text>
-            {(loading && <Text>Loading..</Text>) || (
-              <FlatList data={handleData} renderItem={renderItem} />
-            )}
+            {(loading && <Text>Loading..</Text>) ||
+              (errorMessage && <Text>city not found..</Text>)}
+
+            {<FlatList data={handleData} renderItem={renderItem} />}
           </View>
         )}
       </View>
